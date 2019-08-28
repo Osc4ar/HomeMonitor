d1e0a8f1d88a89f38916c4bc6c587d48d2f96f8b
Osc4ar <oscar_97.10@outlook.com>
2019-08-28 14:25:04 -0500

    Fixing main.dart changes

--- monitor_app/lib/main.dart
+++ monitor_app/lib/main.dart
@@ -8,17 +8,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Home Monitor',
       theme: ThemeData(
-<<<<<<< HEAD
         brightness: Brightness.dark,
         fontFamily: 'Montserrat',
         primaryColor: Colors.black,
         accentColor: Colors.white,
-=======
-        brightness: Brightness.light,
-        fontFamily: 'Montserrat',
-        primaryColor: Colors.white,
-        accentColor: Colors.black,
->>>>>>> fc5827c8115732eebe2b7243c35c04e749adbbf9
       ),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Home Monitor'),
@@ -35,7 +28,6 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-<<<<<<< HEAD
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   List<Tab> deviceTabs = <Tab>[
@@ -51,17 +43,6 @@ class _MyHomePageState extends State<MyHomePage>
 
   TabController _tabController;
 
-=======
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
-
-  List<Tab> deviceTabs = <Tab>[
-    Tab(text: 'Oficina',),
-    Tab(text: 'Casa',),
-  ];
-
-  TabController _tabController;
-
->>>>>>> fc5827c8115732eebe2b7243c35c04e749adbbf9
   @override
   void initState() {
     super.initState();
@@ -80,7 +61,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
-<<<<<<< HEAD
       ),
       body: Center(
         child: Text(
@@ -88,25 +68,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           style: TextStyle(fontSize: 36),
         ),
       ),
-=======
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: deviceTabs,
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: deviceTabs.map((Tab tab) {
-          final String label = tab.text.toLowerCase();
-          return Center(
-            child: Text(
-              'This is the $label tab',
-              style: TextStyle(fontSize: 36),
-            ),
-          );
-        }).toList(),
-      ),
->>>>>>> fc5827c8115732eebe2b7243c35c04e749adbbf9
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
@@ -157,7 +118,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         onPressed: () {},
         tooltip: 'Tomar fotografía',
         child: Icon(Icons.camera_alt),
-<<<<<<< HEAD
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
@@ -192,9 +152,6 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           ),
         ),
       ),
-=======
-      ), // This trailing comma makes auto-formatting nicer for build methods.
->>>>>>> fc5827c8115732eebe2b7243c35c04e749adbbf9
     );
   }
 }
