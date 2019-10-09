d861d901fa83513af5e84f22cb6571ceaa162e6c
Osc4ar <oscar_97.10@outlook.com>
2019-10-09 13:28:49 -0500

    Implemented config files

--- rasp/take_picture.py
+++ rasp/take_picture.py
@@ -4,7 +4,7 @@ import time
 import picamera
 import firebase_uploader
 
-def take_picture():
+def take_picture(upload_info):
   start = time.time()
   now = datetime.now()
   ts = int(now.timestamp())
@@ -17,12 +17,21 @@ def take_picture():
     picam.stop_preview()
     picam.close()
   print(f'Fotografia capturada en {time.time()-start} segundos\n')
-  firebase_uploader.upload_file(filename, ts)
+  upload_info['filename'] = filename
+  firebase_uploader.upload_file(upload_info)
 
 if __name__ == "__main__":
+    upload_info = {}
+    user_file = open("user", "r")
+    name_file = open("name", "r")
+    upload_info['uid'] = user_file.read()
+    upload_info['deviceId'] = name_file.read()
+    user_file.close()
+    name_file.close()
+
     while True:
-        if os.path.exists('./take_picture'):
-            take_picture()
-            os.remove('./take_picture')            
-        else:
-            time.sleep(0.1)
+      if os.path.exists('./take_picture'):
+        take_picture(upload_info)
+        os.remove('./take_picture')
+      else:
+        time.sleep(0.1)
